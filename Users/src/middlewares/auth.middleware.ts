994389d75e8507009874749988a49453ff9430fe
Mayank Soni <mayank93soni@gmail.com>
2024-07-11 23:01:21 +0530

    add class for middleware

---
+++ Users/src/middlewares/auth.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+class middleware {
+    // Define middleware functions here
+
+    // auth middleware function
+    static auth(req, res, next) {
+
+    }
+}
+
+
+
