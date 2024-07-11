b669735f80873915ec5884a43d8fc13ca9f78226
Mayank Soni <mayank93soni@gmail.com>
2024-07-11 22:52:30 +0530

    add constant file with few event names

---
+++ Users/src/constant.ts
@@ -0,0 +1,43 @@
+const EMAIL_VERIFY:string =  "EMAIL_VERIFY"
+const EMAIL_VERIFIED:string = "EMAIL_VERIFIED"
+const OK_EMAIL_SENT:string = "OK_EMAIL_SENT"
+const EMAIL_RESEND:string = "EMAIL_RESEND"
+const NOTIFICATION:string = "NOTIFICATION"
+const NOTIFICATION_SUCCESS:string = "NOTIFICATION_SUCCESS"
+const OK:string = "OK"
+const FAILED:string = "FAILED"
+const CREATE:string = "CREATE"
+const CREATED:string = "CREATED"
+const UPDATE:string = "UPDATE"
+const SOFT_DELETE:string = "SOFT_DELETE"
+const PAYMENT:string = "PAYMENT"
+const REJECT:string = "REJECT"
+const ACCEPT:string = "ACCEPT"
+const ACCESS_DENIED:string = "ACCESS_DENIED"
+const ADMIN:string = "ADMIN"
+const ONLY_ADMIN:string = "ONLY_ADMIN"
+const UNKNOWN:string = "UNKNOWN"
+
+export {
+    EMAIL_VERIFY,
+    EMAIL_VERIFIED,
+    OK_EMAIL_SENT,
+    EMAIL_RESEND,
+    NOTIFICATION,
+    NOTIFICATION_SUCCESS,
+    OK,
+    FAILED,
+    CREATE,
+    CREATED,
+    UPDATE,
+    SOFT_DELETE,
+    PAYMENT,
+    REJECT,
+    ACCEPT,
+    ACCESS_DENIED,
+    ADMIN,
+    ONLY_ADMIN,
+    UNKNOWN,
+}
+
+
