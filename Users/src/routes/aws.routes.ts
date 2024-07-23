8876b9ead8684e63c3ce07a19897bc305f34fa00
Mayank Soni <mayank93soni@gmail.com>
2024-07-23 15:35:27 +0530

    add aws routes

--- Users/src/routes/aws.routes.ts
+++ Users/src/routes/aws.routes.ts
@@ -1,5 +1,5 @@
 import express, { Router,  } from "express";
-import { middleware } from "../middlewares/middleware";
+import { middleware } from "../midlewares/middleware";
 import { AWS_SERVICES } from "../helper/AWS"
 import UserService from "../controllers/user.controller";
 import {AWSAPI} from "../controllers/aws.controller"
