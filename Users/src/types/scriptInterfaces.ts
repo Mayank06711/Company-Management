62b8b99e869f37946bd28afa562464246dc5e620
Mayank Soni <mayank93soni@gmail.com>
2024-08-09 00:09:23 +0530

    add eventData interface

--- Users/src/types/scriptInterfaces.ts
+++ Users/src/types/scriptInterfaces.ts
@@ -1,6 +1,22 @@
 // emailOptions.ts
-export interface EmailOptions {
-    email: string;
-    subject: string;
-    message: string;
+export  interface EventData {
+    email?: string;
+    subject?: string;
+    req?: any; // Optional request object
+    data?: Record<string, any>; // Optional additional data
+    message: string; // Message related to the event
+  }
+
+ export interface PriorityEvent {
+  name: string;
+  data?: EventData;
+  priority?: number | 0;
+  retries?: number | 0;
 }
+  
+/*
+Record<K, T> Utility Type:
+
+Record is a TypeScript utility type that constructs an object type with properties of type T using keys of type K.
+The general form is Record<K, T>, where K is the type of the keys and T is the type of the values.
+*/
