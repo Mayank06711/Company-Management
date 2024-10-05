e6f2b3d9e70f65a7ced1d01f83a26bc9f28e8ea5
Mayank Soni <mayank93soni@gmail.com>
2024-10-05 05:32:50 +0530

    resolved final conflict

--- Users/src/controllers/employee.controller.ts
+++ Users/src/controllers/employee.controller.ts
@@ -6,7 +6,6 @@ import AsyncHandler from "../utils/asyncHandler";
 import { EmployeeSchema } from "../models/zodValidation.schemas";
 import { AuthServices } from "../helper/auth";
 import UserService from "./user.controller";
-<<<<<<< HEAD
 import DepartmentService from "./department.controller";
 import { newRequest } from "../types/express";
 import EmitEvents from "../utils/eventEmitter";
@@ -174,92 +173,6 @@ class Employees {
       req.user?.role!== "Manager"
     ) {
       throw new ApiError(403, "You are not allowed to remove employee data");
-=======
-import {newRequest} from "../types/express"
-const roles =  ["HR" , "CEO" , "DIRECOR" ]
-
-class Employees {
-  private static async createEmployee(req: newRequest, res: Response): Promise<void> {
-
-     if(!roles.includes(req.user.role) ){{
-      throw new ApiError(401 , "You cannot create Employee")
-     }}
-    const validationResult = EmployeeSchema.safeParse(req.body);
-    
-    if (!validationResult.success) {
-      throw new ApiError(400, `${validationResult.error.errors}`);
-    }
-    const Employee = await prisma.user.findFirst({
-      where:{
-          username: validationResult.data.userId,
-      },
-  })
-  if(Employee){
-      throw new ApiError(400, "Employee already exists");
-  }
-
-   
-    const newEmployee = await prisma.employee.create({
-      data :req.body
-    });
-
-    if(!newEmployee){
-      throw new ApiError(400 , "Employee Creation Failed");
-    }
-
-    res.status(201).json(newEmployee);
-  }
-    
-    
-    private static async updateEmplSalary(req:newRequest, res:Response){
-      if(!roles.includes(req.user.role) ){{
-        throw new ApiError(401 , "You cannot update Employee")
-       }}
-
-      const update = req.body;
-      
-      const updateUser = await prisma.employee.update({
-        where:{ 
-             id: req.body?.id
-     },
-        data:{
-          salary:update.salary
-        }
-     });
-    if(updateUser){
-        throw new ApiError(400, "Failed updating salary");
-    }
-  
-      res.status(201).json(updateUser);;
-    }
-
-    private static async promoteEmploy(req:newRequest, res:Response){
-      if(!roles.includes(req.user.role) ){{
-        throw new ApiError(401 , "You cannot promote Employee")
-       }}
-
-       const{ position , positionDesc}= req.body;
-       const updateUser = await prisma.employee.update({
-        where:{ 
-             id: req.body?.id
-     },
-        data:{
-          position,
-          positionDesc
-        }
-     })
-
-    if(!updateUser){
-      throw new ApiError(401 , "Failed to update position");
-    } 
-
-    res.status(200).json((updateUser));
-
-    }
-    
-    private static async removeEmploy(req:Request, res:Response){
-      
->>>>>>> ddf76d85eae97ce49613666097abc51445c45aa7
     }
 
     const employee = await prisma.employee.findFirst({
@@ -267,7 +180,6 @@ class Employees {
       include: { user: true },
     });
 
-<<<<<<< HEAD
     const result = await prisma.employee.update({
       where: { id: employeeId },
       data: { isActive: false },
@@ -425,9 +337,5 @@ class Employees {
   static sendReports = Employees.sendReport
   static markAttendance = Employees.attendance
   static DataSharing = Employees.shareData;
-=======
-    static newEmployee  = this.createEmployee;
-    static UpdateSalary = this.updateEmplSalary
->>>>>>> ddf76d85eae97ce49613666097abc51445c45aa7
 }
 export { Employees };
